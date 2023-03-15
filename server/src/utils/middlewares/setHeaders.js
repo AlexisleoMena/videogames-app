7b466698d5f5e43c1b58e7cf825219871d4d8342
AlexisleoMena <alexismena2690@gmail.com>
2023-03-15 13:55:40 -0300

    Arreglamos problema de CORS en el server

--- server/src/utils/middlewares/setHeaders.js
+++ server/src/utils/middlewares/setHeaders.js
@@ -1,6 +1,6 @@
 // Las Cabeceras HTTP son los parámetros que se envían en una petición o respuesta HTTP al cliente o al servidor para proporcionar información esencial sobre la transacción en curso. Estas  cabeceras proporcionan información mediante la sintaxis 'Cabecera: Valor' y son enviadas automáticamente por el navegador o el servidor Web.
 function setHeaders(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
